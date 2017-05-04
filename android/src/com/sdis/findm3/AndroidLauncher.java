d25fd2df64ae80133cb29343ebc640ad05fa4341
Jonas L <joaohccl@live.com.pt>
2017-05-04 15:52:36 +0100

    Libgdx added

---
+++ android/src/com/sdis/findm3/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.sdis.findm3;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sdis.findm3.FindApp;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new FindApp(), config);
+	}
+}
